6a34a18c54573a1c73796674027d6a387c898725
Coffeeteer <tgrieshiemer@gmail.com>
2016-07-28 15:23:35 -0400

    setting up routes

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,2 +1,9 @@
 //A GET Route to display the survey page.
+app.get('./public/survey.html', function(req,res) {
+    res.render('Hello I\'m here.');//'index', survey[0]);
+});
+
 //A default USE route that leads to home.html which displays the home page.
+app.get('./public/home.html', function(req,res) {
+    res.render('Hello, I\'m home!');//'index', survey[0]);
+});
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var handlebars = require('handlebars');
+
+app.listen(3000);
 
