fed7e6a1a0a9f82d4e097a6e483657e297d52705
Coffeeteer <tgrieshiemer@gmail.com>
2016-07-28 16:47:19 -0400

    added repositoyr field in JSON package

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var handlebars = require('handlebars');
 
-app.listen(3000);
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 console.log('RUNNING, like Forest');
+
+
+// var fs = require('fs');
+
+// app.listen(3000);
+
+// fs.readFile('friends.js', function(err, data) {
+// 	if(err) {
+// 		console.log(err, 'error');
+// 	}else{
+// 		console.log('RUNNING, like Forest');
+// 	}	
+// });
