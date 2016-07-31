fe7610ec0d76afefce4d09f0b14af7a0224c8064
Coffeeteer <tgrieshiemer@gmail.com>
2016-07-31 14:16:23 -0400

    put questions in HTML file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,15 @@
-var survey = [
-  {survey: '1. You want to be ridiculously famous, with people of all walks of life fawning all over you.'},
-  {survey: '2. You often sing in the shower at the top of your lungs, and the neighbots have complained.'},
-  {survey: '3. You have close relationship with your family, if "Mom" isn\'t tatooed on your arm you secretly want it.'},
-  {survey: '4. You would rather have an dependable nine-to-five type job that\'s pays well, rather than the uncertainty of a startup.'},
-  {survey: '5. You consider yourself a country bumkin and love the fresh air, and open space of country living.'},
-  {survey: '6. Your ultimate dream is to become a pokemon master, you just gotta catch \'em all!'},
-  {survey: '7. Healthy eating, exercise, and staying active is esstential to your life.'},
-  {survey: '8. No one is more social than you are, you\'ll strike up a conversation with strangers on the street.'},
-  {survey: '9. You hate the news because everything is negative and because you can do nothing to change the course of events.'},
-  {survey: '10. Your exes eften told you how "clingy" you are.'}
-];
+// var survey = [
+//   {survey: '1. You want to be ridiculously famous, with people of all walks of life fawning all over you.'},
+//   {survey: '2. You often sing in the shower at the top of your lungs, and the neighbots have complained.'},
+//   {survey: '3. You have close relationship with your family, if "Mom" isn\'t tatooed on your arm you secretly want it.'},
+//   {survey: '4. You would rather have an dependable nine-to-five type job that\'s pays well, rather than the uncertainty of a startup.'},
+//   {survey: '5. You consider yourself a country bumkin and love the fresh air, and open space of country living.'},
+//   {survey: '6. Your ultimate dream is to become a pokemon master, you just gotta catch \'em all!'},
+//   {survey: '7. Healthy eating, exercise, and staying active is esstential to your life.'},
+//   {survey: '8. No one is more social than you are, you\'ll strike up a conversation with strangers on the street.'},
+//   {survey: '9. You hate the news because everything is negative and because you can do nothing to change the course of events.'},
+//   {survey: '10. Your exes eften told you how "clingy" you are.'}
+// ];
 
 {
   "name":"Ahmed",
